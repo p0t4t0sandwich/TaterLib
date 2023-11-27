2f15e14d2b0b658007a8ff824be85b4b1ff7d477
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-11-26 18:45:37 -0600

    Improved project organization

--- common/src/main/java/dev/neuralnexus/taterlib/common/TaterLib.java
+++ common/src/main/java/dev/neuralnexus/taterlib/common/TaterLib.java
@@ -142,7 +142,7 @@ public class TaterLib {
     public static class Constants {
         public static final String PROJECT_NAME = "TaterLib";
         public static final String PROJECT_ID = "taterlib";
-        public static final String PROJECT_VERSION = "1.1.0-R0.11-SNAPSHOT";
+        public static final String PROJECT_VERSION = "1.1.0-R0.12-SNAPSHOT";
         public static final String PROJECT_AUTHORS = "p0t4t0sandwich";
         public static final String PROJECT_DESCRIPTION = "A cross API code library for various generalizations used in the Tater* plugins";
         public static final String PROJECT_URL = "https://github.com/p0t4t0sandwich/TaterLib";
