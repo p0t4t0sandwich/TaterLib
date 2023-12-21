18aef0bc6cd783f5dca1d20c77523b8b997ca78a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-12-21 15:52:53 -0600

    Backported Forge to 1.11

---
+++ forge-legacy/forge-1.11/src/main/java/dev/neuralnexus/taterlib/forge/inventory/ForgeItemMeta.java
@@ -0,0 +1,77 @@
+package dev.neuralnexus.taterlib.forge.inventory;
+
+import dev.neuralnexus.taterlib.inventory.ItemMeta;
+
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+/** Abstracts a Forge item meta to an AbstractItemMeta. */
+public class ForgeItemMeta implements ItemMeta {
+    private final ItemStack itemStack;
+
+    /**
+     * Constructor.
+     *
+     * @param itemStack The Forge item stack.
+     */
+    public ForgeItemMeta(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean hasDisplayName() {
+        return itemStack.hasDisplayName();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getDisplayName() {
+        return itemStack.getDisplayName();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setDisplayName(String name) {
+        itemStack.setStackDisplayName(name);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean hasLore() {
+        // TODO: Implement
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public List<String> getLore() {
+        // TODO: Implement
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setLore(List<String> lore) {
+        // TODO: Implement
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean hasEnchants() {
+        return itemStack.isItemEnchanted();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isUnbreakable() {
+        return itemStack.isItemStackDamageable();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setUnbreakable(boolean unbreakable) {
+        // TODO: Implement
+    }
+}
