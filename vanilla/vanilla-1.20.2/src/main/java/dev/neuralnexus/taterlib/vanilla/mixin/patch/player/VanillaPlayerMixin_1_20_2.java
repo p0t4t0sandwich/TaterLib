31d417ce907a025573d3ed6cb358c716b5e3a81a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-01-21 20:38:23 -0600

    Progress, still need to debug Fabric a tad

--- vanilla/vanilla-1.20.2/src/main/java/dev/neuralnexus/taterlib/vanilla/mixin/patch/player/VanillaPlayerMixin_1_20_2.java
+++ vanilla/vanilla-1.20.2/src/main/java/dev/neuralnexus/taterlib/vanilla/mixin/patch/player/VanillaPlayerMixin_1_20_2.java
@@ -4,24 +4,19 @@ import dev.neuralnexus.taterlib.vanilla.player.VanillaPlayer;
 
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(VanillaPlayer.class)
 public abstract class VanillaPlayerMixin_1_20_2 {
-    @Shadow @Final private Player player;
-
     /**
      * @author Dylan Sperrer (p0t4t0sandwich)
      * @reason Patch for 1.20.2
      */
     @Overwrite
     public void sendPluginMessage(String channel, byte[] data) {
-        ((ServerPlayer) player)
+        ((ServerPlayer) ((VanillaPlayer) (Object) this).getPlayer())
                 .connection.send(
                         new ClientboundCustomPayloadPacket(
                                 new VanillaCustomPacketPayload_1_20_2(channel, data)));
@@ -33,6 +28,6 @@ public abstract class VanillaPlayerMixin_1_20_2 {
      */
     @Overwrite
     public int getPing() {
-        return ((ServerPlayer) player).connection.latency();
+        return ((ServerPlayer) ((VanillaPlayer) (Object) this).getPlayer()).connection.latency();
     }
 }
