b070421b2f105bb9d7bf47337569ecca2151ae5a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-02-04 22:55:21 -0600

    Implemented Fabric 1.19.x

---
+++ fabric/fabric-1.19.3/src/main/java/dev/neuralnexus/taterlib/v1_19_3/vanilla/fabric/mixin/patch/player/VanillaPlayerMixin_1_19_3.java
@@ -0,0 +1,45 @@
+package dev.neuralnexus.taterlib.v1_19_3.vanilla.fabric.mixin.patch.player;
+
+import dev.neuralnexus.taterlib.utils.Location;
+import dev.neuralnexus.taterlib.v1_19.vanilla.player.VanillaPlayer;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+/** Patch mixin for VanillaPlayer 1.19.3. */
+@Mixin(value = VanillaPlayer.class, remap = false)
+public class VanillaPlayerMixin_1_19_3 {
+    /**
+     * @author Dylan Sperrer (p0t4t0sandwich)
+     * @reason Patch for 1.19.3
+     */
+    @Overwrite
+    public void setSpawn(Location location, boolean forced) {
+        // TODO: Abstract world information
+        ResourceLocation resourceLocation;
+        String[] resourceString = location.getWorld().split(":");
+        if (resourceString.length != 1) {
+            resourceLocation = new ResourceLocation(resourceString[0], resourceString[1]);
+        } else {
+            resourceLocation = new ResourceLocation(resourceString[0]);
+        }
+        ResourceKey<Level> dimension = ResourceKey.create(Registries.DIMENSION, resourceLocation);
+        ((ServerPlayer) ((VanillaPlayer) (Object) (this)).getPlayer())
+                .setRespawnPosition(
+                        dimension,
+                        new BlockPos(
+                                (int) location.getX(),
+                                (int) location.getY(),
+                                (int) location.getZ()),
+                        0,
+                        forced,
+                        false);
+    }
+}
---
+++ fabric/fabric-1.19/src/main/java/dev/neuralnexus/taterlib/v1_19/vanilla/fabric/mixin/listeners/server/ServerStartedMixin_1_19.java
@@ -0,0 +1,28 @@
+package dev.neuralnexus.taterlib.v1_19.vanilla.fabric.mixin.listeners.server;
+
+import dev.neuralnexus.taterlib.event.api.ServerEvents;
+import dev.neuralnexus.taterlib.v1_19.vanilla.event.server.VanillaServerStartedEvent;
+
+import net.minecraft.server.MinecraftServer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/** Mixin for the server started listener. */
+@Mixin(MinecraftServer.class)
+public class ServerStartedMixin_1_19 {
+    /** Called when the server has started. */
+    @Inject(
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lnet/minecraft/server/MinecraftServer;updateStatusIcon(Lnet/minecraft/network/protocol/status/ServerStatus;)V",
+                            ordinal = 0),
+            method = "runServer")
+    private void onServerStarted(CallbackInfo info) {
+        ServerEvents.STARTED.invoke(new VanillaServerStartedEvent((MinecraftServer) (Object) this));
+    }
+}
