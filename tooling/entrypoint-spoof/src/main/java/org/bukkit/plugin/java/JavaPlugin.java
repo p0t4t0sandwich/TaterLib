f18430df5653c1b28a5326fefc132e44e225c71a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-02-19 02:48:39 -0600

    - Updated entrypoint-spoof

--- tooling/entrypoint-spoof/src/main/java/org/bukkit/plugin/java/JavaPlugin.java
+++ tooling/entrypoint-spoof/src/main/java/org/bukkit/plugin/java/JavaPlugin.java
@@ -1,10 +1,16 @@
 package org.bukkit.plugin.java;
 
+import org.bukkit.Server;
+
 import java.util.logging.Logger;
 
 /** Fake Bukkit abstract Plugin class to simplify the creation of entrypoints. */
 public abstract class JavaPlugin extends PluginBase {
     public Logger getLogger() {
+        return null;
+    }
+
+    public Server getServer() {
         return null;
     }
 
