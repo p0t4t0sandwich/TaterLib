af4d7cd12ad496ee105fd3cd253bb003098b1d3a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-06-01 16:32:45 -0600

    Fixed a slight typo in the Forge version entrypoint resolution

--- tooling/loader/src/main/java/dev/neuralnexus/taterlib/loader/platforms/ForgeLoaderPlugin.java
+++ tooling/loader/src/main/java/dev/neuralnexus/taterlib/loader/platforms/ForgeLoaderPlugin.java
@@ -58,7 +58,7 @@ public class ForgeLoaderPlugin {
             version = "." + MinecraftVersion.V1_17_1.getDelimiterString();
         } else if (mcv.isInRange(true, MinecraftVersion.V1_18, true, MinecraftVersion.V1_18_2)) {
             version = "." + MinecraftVersion.V1_18.getDelimiterString();
-        } else if (mcv.isInRange(true, MinecraftVersion.V1_19, true, MinecraftVersion.V1_19_3)) {
+        } else if (mcv.isInRange(true, MinecraftVersion.V1_19, true, MinecraftVersion.V1_19_4)) {
             version = "." + MinecraftVersion.V1_19.getDelimiterString();
         } else if (mcv.isInRange(true, MinecraftVersion.V1_20, true, MinecraftVersion.V1_20_4)) {
             version = "." + MinecraftVersion.V1_20_2.getDelimiterString();
