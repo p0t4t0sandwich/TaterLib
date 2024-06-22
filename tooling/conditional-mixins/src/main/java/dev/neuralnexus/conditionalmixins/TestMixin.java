aca926d78b5f793adc9d6e86792e65020f3f6259
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-06-21 21:20:11 -0600

    Conditional Mixins shades and minifies loader for the extra data

--- tooling/conditional-mixins/src/main/java/dev/neuralnexus/conditionalmixins/TestMixin.java
+++ tooling/conditional-mixins/src/main/java/dev/neuralnexus/conditionalmixins/TestMixin.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+
 package dev.neuralnexus.conditionalmixins;
 
 import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
@@ -7,9 +13,7 @@ import dev.neuralnexus.taterlib.loader.api.Platform;
 
 import org.spongepowered.asm.mixin.Mixin;
 
-@ReqMCVersion(
-        min = MinecraftVersion.V1_17_1,
-        max = MinecraftVersion.V1_18)
+@ReqMCVersion(min = MinecraftVersion.V1_17_1, max = MinecraftVersion.V1_18)
 @ReqPlatform(Platform.FABRIC)
 @Mixin(String.class)
 public class TestMixin {}
