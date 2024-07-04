9becf491b26f04088c935a6dcd1c4dc47102744d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-07-03 20:51:19 -0600

    Fixed velocity event subscription in entrypoint-spoof

--- tooling/entrypoint-spoof/src/main/java/com/velocitypowered/api/event/Subscribe.java
+++ tooling/entrypoint-spoof/src/main/java/com/velocitypowered/api/event/Subscribe.java
@@ -1,4 +1,11 @@
 package com.velocitypowered.api.event;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /** Fake Subscribe annotation for Velocity, so we can subscribe to basic init/shutdown events. */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Subscribe {}
