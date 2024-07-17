bb654f0395046ffb1e3695655b5d3c919bfba87b
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-07-16 18:43:39 -0600

    Sorta got Forge 1.16 mojmaps working with Forge

---
+++ forge/forge-1.16/src/main/java/dev/neuralnexus/taterlib/v1_16_3/forge/hooks/permissions/ForgePermissionsHook.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+
+package dev.neuralnexus.taterlib.v1_16_3.forge.hooks.permissions;
+
+import dev.neuralnexus.taterapi.entity.Permissible;
+import dev.neuralnexus.taterapi.entity.player.Player;
+import dev.neuralnexus.taterapi.hooks.permissions.PermissionsHook;
+
+import dev.neuralnexus.taterlib.v1_16.vanilla.command.VanillaCommandSender;
+import dev.neuralnexus.taterlib.v1_16.vanilla.entity.player.VanillaPlayer;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.server.permission.PermissionAPI;
+
+/** A hook for Forge permissions */
+public class ForgePermissionsHook implements PermissionsHook {
+    @Override
+    public String name() {
+        return "forgepermissions";
+    }
+
+    @Override
+    public boolean hasPermission(Permissible permissible, String permission) {
+        if (permissible.hasPermission(4)) {
+            return true;
+        }
+
+        PlayerEntity player;
+        if (permissible instanceof Player) {
+            player = ((VanillaPlayer) permissible).player();
+        } else {
+            CommandSource source = ((VanillaCommandSender) permissible).sender();
+            if (source.getEntity() == null) {
+                return false;
+            }
+            player = (PlayerEntity) source.getEntity();
+        }
+        return PermissionAPI.getPermissionHandler()
+                .hasPermission(player.getGameProfile(), permission, null);
+    }
+}
---
+++ forge/forge-1.16/src/main/java/dev/neuralnexus/taterlib/v1_16_3/forge/listeners/server/ForgeServerListener.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+
+package dev.neuralnexus.taterlib.v1_16_3.forge.listeners.server;
+
+import dev.neuralnexus.taterapi.event.api.ServerEvents;
+import dev.neuralnexus.taterlib.v1_16.vanilla.event.server.VanillaServerStartedEvent;
+import dev.neuralnexus.taterlib.v1_16.vanilla.event.server.VanillaServerStartingEvent;
+import dev.neuralnexus.taterlib.v1_16.vanilla.event.server.VanillaServerStoppedEvent;
+import dev.neuralnexus.taterlib.v1_16.vanilla.event.server.VanillaServerStoppingEvent;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
+
+/** Listens for server events. */
+public class ForgeServerListener {
+    /**
+     * Called when the server starts.
+     *
+     * @param event The server starting event
+     */
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        ServerEvents.STARTING.invoke(new VanillaServerStartingEvent(event.getServer()));
+    }
+
+    /**
+     * Called when the server starts.
+     *
+     * @param event The server started event
+     */
+    @SubscribeEvent
+    public void onServerStarted(FMLServerStartedEvent event) {
+        ServerEvents.STARTED.invoke(new VanillaServerStartedEvent(event.getServer()));
+    }
+
+    /**
+     * Called when the server stops.
+     *
+     * @param event The server stopping event
+     */
+    @SubscribeEvent
+    public void onServerStopping(FMLServerStoppingEvent event) {
+        ServerEvents.STOPPING.invoke(new VanillaServerStoppingEvent(event.getServer()));
+    }
+
+    /**
+     * Called when the server stops.
+     *
+     * @param event The server stopped event
+     */
+    @SubscribeEvent
+    public void onServerStopped(FMLServerStoppedEvent event) {
+        ServerEvents.STOPPED.invoke(new VanillaServerStoppedEvent(event.getServer()));
+    }
+}
