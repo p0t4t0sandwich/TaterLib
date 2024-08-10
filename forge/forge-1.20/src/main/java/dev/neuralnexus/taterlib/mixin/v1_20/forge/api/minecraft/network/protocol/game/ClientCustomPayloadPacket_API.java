a995a322235394fdfd3d9b3f0cad6c9288a72894
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-10 12:05:29 -0600

    Added ClientCustomPayloadPacket API interface mixins

---
+++ forge/forge-1.20/src/main/java/dev/neuralnexus/taterlib/mixin/v1_20/forge/api/minecraft/network/protocol/game/ClientCustomPayloadPacket_API.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_20.forge.api.minecraft.network.protocol.game;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+import dev.neuralnexus.taterapi.network.CustomPayloadPacket;
+import dev.neuralnexus.taterapi.resource.ResourceKey;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import net.minecraft.resources.ResourceLocation;
+
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Interface.Remap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@ReqMappings(Mappings.SEARGE)
+@ReqMCVersion(min = MinecraftVersion.V1_20, max = MinecraftVersion.V1_20_1)
+@Mixin(ClientboundCustomPayloadPacket.class)
+@Implements(@Interface(iface = CustomPayloadPacket.class, prefix = "packet$", remap = Remap.NONE))
+public abstract class ClientCustomPayloadPacket_API {
+    @Shadow
+    public abstract ResourceLocation shadow$getIdentifier();
+
+    @Shadow
+    public abstract FriendlyByteBuf shadow$getData();
+
+    public ResourceKey packet$channel() {
+        return (ResourceKey) this.shadow$getIdentifier();
+    }
+
+    public byte[] packet$data() {
+        return this.shadow$getData().array();
+    }
+}
