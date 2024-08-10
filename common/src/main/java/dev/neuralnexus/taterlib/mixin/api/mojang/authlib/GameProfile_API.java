1d002dbb642dcb07e1c8eb70ce5ce3e3d3c78ecd
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-10 00:20:28 -0600

    Added api cast conversions for client player/server

---
+++ common/src/main/java/dev/neuralnexus/taterlib/mixin/api/mojang/authlib/GameProfile_API.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.api.mojang.authlib;
+
+import com.mojang.authlib.GameProfile;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+import dev.neuralnexus.taterapi.command.CommandSender;
+import dev.neuralnexus.taterapi.entity.Permissible;
+import dev.neuralnexus.taterapi.entity.player.SimplePlayer;
+
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Interface.Remap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.UUID;
+
+@ReqMCVersion(min = MinecraftVersion.V1_14)
+@Mixin(value = GameProfile.class, remap = false)
+@Implements({
+        @Interface(iface = CommandSender.class, prefix = "cmdSender$", remap = Remap.NONE),
+        @Interface(iface = Permissible.class, prefix = "permissible$", remap = Remap.NONE),
+        @Interface(iface = SimplePlayer.class, prefix = "simplePlayer$", remap = Remap.NONE)
+})
+public abstract class GameProfile_API {
+    @Shadow public abstract String shadow$getName();
+
+    @Shadow public abstract UUID shadow$getId();
+
+    public String cmdSender$name() {
+        return this.shadow$getName();
+    }
+
+    public void cmdSender$sendMessage(String message) {
+        // TODO: Some abstract way to wrap around and send a message.
+        //   Needs to be as such in order to keep this class platform-agnostic.
+        //   Might update SimplePlayer to send messages to a specific player.
+        //   That way TaterAPIProvider can be used to dispatch the message.
+    }
+
+    public String simplePlayer$displayName() {
+        return this.cmdSender$name();
+    }
+
+    public UUID permissible$uuid() {
+        return this.shadow$getId();
+    }
+
+    public boolean permissible$hasPermission(int permissionLevel) {
+        return false;
+    }
+}
+
---
+++ forge/forge-1.20.2/src/main/java/dev/neuralnexus/taterlib/mixin/v1_20_2/forge/api/minecraft/client/Minecraft_API.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_20_2.forge.api.minecraft.client;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+import dev.neuralnexus.taterapi.entity.player.SimplePlayer;
+import dev.neuralnexus.taterapi.server.SimpleServer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.player.LocalPlayer;
+
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Interface.Remap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jetbrains.annotations.Nullable;
+
+@ReqMappings(Mappings.SEARGE)
+@ReqMCVersion(min = MinecraftVersion.V1_20_2, max = MinecraftVersion.V1_20_4)
+@Mixin(Minecraft.class)
+@Implements(@Interface(iface = SimpleServer.class, prefix = "server$", remap = Remap.NONE))
+public abstract class Minecraft_API {
+    @Shadow @Nullable public LocalPlayer player;
+
+    @Shadow
+    @Nullable public abstract ClientPacketListener shadow$getConnection();
+
+    public String server$brand() {
+        if (this.shadow$getConnection() == null) return "Local";
+        String brand = this.shadow$getConnection().serverBrand();
+        return brand == null ? "Local" : brand;
+    }
+
+    public List<SimplePlayer> server$onlinePlayers() {
+        return this.shadow$getConnection().getOnlinePlayers().stream()
+                .map(PlayerInfo::getProfile)
+                .map(SimplePlayer.class::cast)
+                .collect(Collectors.toList());
+    }
+
+    void server$broadcastMessage(String message) {
+        this.shadow$getConnection().sendChat(message);
+    }
+}
