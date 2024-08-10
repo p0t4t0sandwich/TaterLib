275d4710f3792ffa947f7a1c3b427a5b6a59a458
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-10 00:20:58 -0600

    Added S2C custom packet listener

---
+++ vanilla/vanilla-1.19/src/main/java/dev/neuralnexus/taterlib/mixin/v1_19/vanilla/listeners/network/S2CCustomPayloadMixin.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_19.vanilla.listeners.network;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+import dev.neuralnexus.taterapi.event.api.NetworkEvents;
+import dev.neuralnexus.taterapi.event.network.impl.S2CCustomPacketEventImpl;
+import dev.neuralnexus.taterapi.network.CustomPayloadPacket;
+import dev.neuralnexus.taterapi.server.SimpleServer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@ReqMappings(Mappings.MOJMAP)
+@ReqMCVersion(min = MinecraftVersion.V1_19, max = MinecraftVersion.V1_19_4)
+@Mixin(ClientPacketListener.class)
+public abstract class S2CCustomPayloadMixin {
+    /**
+     * Called when a custom payload packet is received from the server.
+     *
+     * @param packet The packet.
+     * @param ci The callback info.
+     */
+    @Inject(method = "handleCustomPayload", at = @At("HEAD"))
+    public void onS2CCustomPacket(ClientboundCustomPayloadPacket packet, CallbackInfo ci) {
+        CustomPayloadPacket customPacket = (CustomPayloadPacket) packet;
+        SimpleServer server = (SimpleServer) Minecraft.getInstance();
+        NetworkEvents.S2C_CUSTOM_PACKET.invoke(new S2CCustomPacketEventImpl(customPacket, server));
+    }
+}
---
+++ vanilla/vanilla-1.21/src/main/java/dev/neuralnexus/taterlib/mixin/v1_21/vanilla/listeners/network/S2CCustomPayloadMixin.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_21.vanilla.listeners.network;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+import dev.neuralnexus.taterapi.event.api.NetworkEvents;
+import dev.neuralnexus.taterapi.event.network.impl.S2CCustomPacketEventImpl;
+import dev.neuralnexus.taterapi.network.CustomPayloadPacket;
+import dev.neuralnexus.taterapi.server.SimpleServer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
+import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@ReqMappings(Mappings.MOJMAP)
+@ReqMCVersion(min = MinecraftVersion.V1_21)
+@Mixin(ClientCommonPacketListenerImpl.class)
+public abstract class S2CCustomPayloadMixin {
+    /**
+     * Called when a custom payload packet is received from the server.
+     *
+     * @param packet The packet.
+     * @param ci The callback info.
+     */
+    @Inject(
+            method =
+                    "handleCustomPayload(Lnet/minecraft/network/protocol/common/ClientboundCustomPayloadPacket;)V",
+            at = @At("HEAD"))
+    @SuppressWarnings("DataFlowIssue")
+    public void onS2CCustomPacket(ClientboundCustomPayloadPacket packet, CallbackInfo ci) {
+        CustomPayloadPacket customPacket = (CustomPayloadPacket) (Object) packet;
+        SimpleServer server = (SimpleServer) Minecraft.getInstance();
+        NetworkEvents.S2C_CUSTOM_PACKET.invoke(new S2CCustomPacketEventImpl(customPacket, server));
+    }
+}
