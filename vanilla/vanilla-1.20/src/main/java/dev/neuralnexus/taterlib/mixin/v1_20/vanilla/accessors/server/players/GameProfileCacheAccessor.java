bcd1a1c696420619cd37d60105efccc90d859661
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-18 19:17:20 -0600

    Added whitelist/playercache methods to Vanilla 1.14+

---
+++ vanilla/vanilla-1.20/src/main/java/dev/neuralnexus/taterlib/mixin/v1_20/vanilla/accessors/server/players/GameProfileCacheAccessor.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/Tater-Certified/Overseer/blob/dev/LICENSE">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_20.vanilla.accessors.server.players;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+
+import net.minecraft.server.players.GameProfileCache;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@ReqMappings(Mappings.MOJMAP)
+@ReqMCVersion(min = MinecraftVersion.V1_20, max = MinecraftVersion.V1_20_6)
+@Mixin(GameProfileCache.class)
+public interface GameProfileCacheAccessor {
+    @Accessor("profilesByName")
+    Map<String, ?> accessor$getProfilesbyName();
+}
