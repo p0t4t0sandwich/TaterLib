29c3b06b14ba6336caed5b4269be0796d04c676d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-18 19:17:38 -0600

    Added whitelist/playercache methods to Fabric 1.14+

---
+++ fabric/fabric-1.14.4/src/main/java/dev/neuralnexus/taterlib/mixin/v1_14_4/fabric/bridge/server/players/StoredUserEntryBridge.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/Tater-Certified/Overseer/blob/dev/LICENSE">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_14_4.fabric.bridge.server.players;
+
+import com.mojang.authlib.GameProfile;
+
+import dev.neuralnexus.taterlib.mixin.v1_14_4.fabric.accessors.server.players.StoredUserEntryAccessor;
+
+import net.minecraft.server.players.UserWhiteListEntry;
+
+@SuppressWarnings("unchecked")
+public interface StoredUserEntryBridge {
+    default GameProfile bridge$getUser(UserWhiteListEntry whiteListEntry) {
+        return ((StoredUserEntryAccessor<GameProfile>) whiteListEntry).invoker$getUser();
+    }
+}
