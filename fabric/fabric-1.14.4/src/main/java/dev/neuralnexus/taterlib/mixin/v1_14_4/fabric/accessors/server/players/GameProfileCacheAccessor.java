60a6ab1e2d0d103c192e754ccd8aa085a94e6eea
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-18 19:17:38 -0600

    Added whitelist/playercache methods to Fabric 1.14+

---
+++ fabric/fabric-1.14.4/src/main/java/dev/neuralnexus/taterlib/mixin/v1_14_4/fabric/accessors/server/players/GameProfileCacheAccessor.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
+ * The project is Licensed under <a href="https://github.com/Tater-Certified/Overseer/blob/dev/LICENSE">MIT</a>
+ */
+package dev.neuralnexus.taterlib.mixin.v1_14_4.fabric.accessors.server.players;
+
+import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
+import dev.neuralnexus.conditionalmixins.annotations.ReqMappings;
+import dev.neuralnexus.taterapi.Mappings;
+import dev.neuralnexus.taterapi.MinecraftVersion;
+
+import net.minecraft.server.players.GameProfileCache;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@ReqMappings(Mappings.INTERMEDIARY)
+@ReqMCVersion(min = MinecraftVersion.V1_14)
+@Mixin(GameProfileCache.class)
+public interface GameProfileCacheAccessor {
+    @Accessor("profilesByName")
+    Map<String, ?> accessor$getProfilesbyName();
+}
