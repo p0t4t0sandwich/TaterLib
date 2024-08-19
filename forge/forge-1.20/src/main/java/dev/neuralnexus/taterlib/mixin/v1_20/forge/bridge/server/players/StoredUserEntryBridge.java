1a59315c55e3bf0d7b8069387d2669c288d40ef0
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-08-18 20:44:27 -0600

    Added whitelist/playercache methods to Forge 1.6.4 and 1.13

--- forge/forge-1.20/src/main/java/dev/neuralnexus/taterlib/mixin/v1_20/forge/bridge/server/players/StoredUserEntryBridge.java
+++ forge/forge-1.20/src/main/java/dev/neuralnexus/taterlib/mixin/v1_20/forge/bridge/server/players/StoredUserEntryBridge.java
@@ -1,6 +1,7 @@
 /**
  * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
- * The project is Licensed under <a href="https://github.com/Tater-Certified/Overseer/blob/dev/LICENSE">MIT</a>
+ * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">GPL-3</a>
+ * The API is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
  */
 package dev.neuralnexus.taterlib.mixin.v1_20.forge.bridge.server.players;
 
