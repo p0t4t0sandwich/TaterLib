a8f3533a1ec60173ea86dbbd2cb7b46c029ae2e9
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-11-22 00:01:03 -0600

    Started Platform enum rework

--- tooling/metadata-api/src/main/java/dev/neuralnexus/modapi/metadata/PlatformNew.java
+++ tooling/metadata-api/src/main/java/dev/neuralnexus/modapi/metadata/PlatformNew.java
@@ -1,4 +1,9 @@
 package dev.neuralnexus.modapi.metadata;
 
 public interface PlatformNew {
+    /**
+     * Get the name of the platform.
+     * @return The name of the platform
+     */
+    String name();
 }
--- tooling/metadata-api/src/main/java/dev/neuralnexus/modapi/metadata/Platforms.java
+++ tooling/metadata-api/src/main/java/dev/neuralnexus/modapi/metadata/Platforms.java
@@ -1,4 +1,25 @@
 package dev.neuralnexus.modapi.metadata;
 
-public class Platforms {
+public final class Platforms {
+    private static PlatformNew[] platforms;
+
+    /**
+     * Returns all platforms that are available.
+     * @return An array of all available platforms
+     */
+    public static PlatformNew[] platforms() {
+        if (platforms == null) {
+            platforms = new PlatformNew[] {};
+        }
+        return platforms;
+    }
+
+    /**
+     * Detects all platforms that are available.
+     * @return An array of all detected platforms
+     */
+    static PlatformNew[] detectPlaforms() {
+        // TODO: Implement
+        return null;
+    }
 }
