2da5fdf5eb8f14a932bb623d4c021a85de0ea28e
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-12-20 00:03:49 -0600

    Worked on revamping the version metadata

---
+++ tooling/metadata-api/src/main/java/dev/neuralnexus/modapi/metadata/impl/version/meta/MetaStore.java
@@ -0,0 +1,11 @@
+package dev.neuralnexus.modapi.metadata.impl.version.meta;
+
+import dev.neuralnexus.modapi.metadata.MinecraftVersion;
+
+public final class MetaStore {
+
+    public static MinecraftVersion.Meta decode(byte[] data) {
+        byte protocol = 0b1010101;
+        return null;
+    }
+}
