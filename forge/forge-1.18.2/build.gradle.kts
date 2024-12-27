fad4b0ea522d338dba0f88bbe57505078ab8e77d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-12-27 16:14:22 -0600

    Removed unneeded shading from Forge-1.18.2

--- forge/forge-1.18.2/build.gradle.kts
+++ forge/forge-1.18.2/build.gradle.kts
@@ -1,7 +1,4 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
-    alias(libs.plugins.shadow)
     alias(libs.plugins.unimined)
 }
 
@@ -21,27 +18,14 @@ unimined.minecraft(sourceSets.main.get()) {
     mappings {
         mojmap()
     }
-    defaultRemapJar = false
-    remap(tasks.shadowJar.get()) {
-        prodNamespace("searge")
-        archiveFileName = "${projectId}-forge-${minecraftVersion}-${project.version}.jar"
-    }
 }
 
 dependencies {
     compileOnly(project(":api"))
     compileOnly(project(":common"))
     compileOnly(project(":loader"))
-    compileOnly(project(":vanilla:vanilla-1.18"))
-}
-
-tasks.named<ShadowJar>("shadowJar") {
-    dependencies {
-        include(project(":vanilla:vanilla-1.18"))
-    }
-    relocate("dev.neuralnexus.taterlib.v1_18.vanilla", "dev.neuralnexus.taterlib.v1_18.vanilla.forge")
 }
 
 tasks.build {
-    dependsOn(tasks.named("remapShadowJar"))
+    dependsOn(tasks.named("remapJar"))
 }
