87d61f4e6e4d1410272bd564a19d31873349d990
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-01-02 22:47:50 -0600

    Slight fix

--- modapi/entrypoint-spoof/src/main/java/net/minecraftforge/server/permission/IPermissionHandler.java
+++ modapi/entrypoint-spoof/src/main/java/net/minecraftforge/server/permission/IPermissionHandler.java
@@ -4,8 +4,13 @@ import com.mojang.authlib.GameProfile;
 
 import net.minecraftforge.server.permission.context.IContext;
 
+import net.minecraftforge.server.permission.nodes.PermissionNode;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
 
 public interface IPermissionHandler {
     boolean hasPermission(GameProfile gameProfile, String string, @Nullable IContext iContext);
+
+    Collection<PermissionNode<?>> getRegisteredNodes();
 }
