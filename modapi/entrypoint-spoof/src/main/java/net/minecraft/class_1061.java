b8b8e1e8ab19927b1f34d48c7d4787e9b72f226f
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-01-07 23:56:31 -0600

    Added some MC stubs to entrypoint spoof

---
+++ modapi/entrypoint-spoof/src/main/java/net/minecraft/class_1061.java
@@ -0,0 +1,7 @@
+package net.minecraft;
+
+/**
+ * Required for net.legacyfabric.fabric.api.permission.v1.PermissibleCommandSource
+ * Maps to Legacy Yarn -> net.minecraft.command.CommandSource
+ */
+public class class_1061 {}
---
+++ modapi/entrypoint-spoof/src/main/java/net/minecraft/class_798.java
@@ -0,0 +1,7 @@
+package net.minecraft;
+
+/**
+ * Required for net.legacyfabric.fabric.api.permission.v1.PlayerPermissionsApi
+ * Maps to Legacy Yarn -> net.minecraft.entity.player.ServerPlayerEntity
+ */
+public class class_798 {}
