46172d994373e2bac57f70a625252259a8e37980
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-01-12 23:24:16 -0600

    Added a few yarn stubs to entrypoint spoof

---
+++ modapi/entrypoint-spoof/src/main/java/net/minecraft/class_1297.java
@@ -0,0 +1,7 @@
+package net.minecraft;
+
+/**
+ * Required for me.lucko.fabric.api.permissions.v0.Permissions Maps to Yarn Intermediary
+ * -> net.minecraft.world.Entity
+ */
+public class class_1297 {}
---
+++ modapi/entrypoint-spoof/src/main/java/net/minecraft/class_2172.java
@@ -0,0 +1,7 @@
+package net.minecraft;
+
+/**
+ * Required for me.lucko.fabric.api.permissions.v0.Permissions Maps to Yarn Intermediary
+ * -> net.minecraft.commands.SharedSuggestionProvider
+ */
+public class class_2172 {}
