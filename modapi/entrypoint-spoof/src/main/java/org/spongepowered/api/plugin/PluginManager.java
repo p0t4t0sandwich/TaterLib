fb0280410b2409fa2822d6dc3589300b3e2d2350
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-02-05 17:24:09 -0600

    Slight fix

--- modapi/entrypoint-spoof/src/main/java/org/spongepowered/api/plugin/PluginManager.java
+++ modapi/entrypoint-spoof/src/main/java/org/spongepowered/api/plugin/PluginManager.java
@@ -5,16 +5,14 @@
  */
 package org.spongepowered.api.plugin;
 
-import org.spongepowered.plugin.PluginContainer;
-
 import java.util.Collection;
 import java.util.Optional;
 
 /** Fake Sponge interface */
 public interface PluginManager {
-    Optional<PluginContainer> plugin(String id);
+    Optional<org.spongepowered.plugin.PluginContainer> plugin(String id);
 
-    Optional<PluginContainer> getPlugin(String id);
+    Optional<org.spongepowered.api.plugin.PluginContainer> getPlugin(String id);
 
     Collection<PluginContainer> plugins();
 
