1ab1e08c61bbfc3a21b812a72a154d097c161a02
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-02-08 01:36:11 -0600

    Offloaded shading from API to Common

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
-plugins {
-    alias(libs.plugins.shadow)
-}
-
 base {
     archivesName = "taterapi"
 }
@@ -20,81 +14,32 @@ dependencies {
     implementation("org.bstats:bstats-sponge:3.0.2")
     implementation("org.bstats:bstats-velocity:3.0.2")
 
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
     // MySQL
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("com.mysql:mysql-connector-j:8.3.0")
 
     // Brigadier
-    compileOnly(libs.brigadier)
+    implementation(libs.brigadier)
 
     // Mixin
-    compileOnly(libs.mixin)
-    compileOnly(libs.asm.tree)
+    implementation(libs.mixin)
+    implementation(libs.asm.tree)
 
     // Cross API dependencies
-    compileOnly("net.luckperms:api:5.4")
-    compileOnly("me.lucko:spark-api:0.1-SNAPSHOT")
+    implementation("net.luckperms:api:5.4")
+    implementation("me.lucko:spark-api:0.1-SNAPSHOT")
 
     // Tooling
     compileOnly(project(":modapi:entrypoint-spoof"))
-    compileOnly(variantOf(libs.modapi.crossperms) {
+    implementation(variantOf(libs.modapi.crossperms) {
         classifier("downgraded-8")
     })
-    compileOnly(variantOf(libs.modapi.metadata) {
+    implementation(variantOf(libs.modapi.metadata) {
         classifier("downgraded-8")
     })
 }
 
 java.disableAutoTargetJvm()
-
-tasks.named<ShadowJar>("shadowJar") {
-    dependencies {
-        include(dependency("${group}:${projectId}:${version}"))
-        exclude("META-INF", "META-INF/**")
-        exclude("LICENSE")
-        exclude("INFO_BIN", "INFO_SRC")
-
-        // Config
-        include(dependency("org.spongepowered:configurate-core:4.2.0-SNAPSHOT"))
-        include(dependency("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT"))
-        include(dependency("com.typesafe:config:1.4.1"))
-        include(dependency("io.leangen.geantyref:geantyref:1.3.16"))
-
-        // bStats
-        include(dependency("org.bstats:bstats-base:3.0.2"))
-        include(dependency("org.bstats:bstats-bukkit:3.0.2"))
-        include(dependency("org.bstats:bstats-bungeecord:3.0.2"))
-        include(dependency("org.bstats:bstats-sponge:3.0.2"))
-        include(dependency("org.bstats:bstats-velocity:3.0.2"))
-
-        // MySQL
-        include(dependency("com.zaxxer:HikariCP:4.0.3"))
-        include(dependency("com.mysql:mysql-connector-j:8.3.0"))
-    }
-
-    minimize {
-        exclude(dependency("org.spongepowered:configurate-core:4.2.0-SNAPSHOT"))
-        exclude(dependency("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT"))
-        exclude(dependency("org.bstats:bstats-base:3.0.2"))
-    }
-
-    // Configurate
-    relocate("org.spongepowered.configurate", "dev.neuralnexus.taterlib.lib.configurate")
-    relocate("com.typesafe.config", "dev.neuralnexus.taterlib.lib.typesafe.config")
-    relocate("io.leangen.geantyref", "dev.neuralnexus.taterlib.lib.geantyref")
-
-    // bStats
-    relocate("org.bstats", "dev.neuralnexus.taterlib.lib.bstats") {
-        exclude("org.bstats.sponge.*")
-    }
-
-    // MySQL
-    relocate("com.zaxxer.hikari", "dev.neuralnexus.taterlib.lib.hikari")
-    relocate("com.mysql", "dev.neuralnexus.taterlib.lib.mysql")
-
-    archiveClassifier.set("full")
-}
-
-tasks.assemble {
-    dependsOn(tasks.shadowJar)
-}
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,17 +10,25 @@ base {
 
 dependencies {
     // Configurate
-    compileOnly("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT")
-    compileOnly("io.leangen.geantyref:geantyref:1.3.15")
+    implementation("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT")
+    implementation("io.leangen.geantyref:geantyref:1.3.15")
 
     // bStats
-    compileOnly("org.bstats:bstats-base:3.0.2")
-
-    // MCLogs
-    implementation("gs.mclo:api:3.0.1")
+    implementation("org.bstats:bstats-base:3.0.2")
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+    implementation("org.bstats:bstats-bungeecord:3.0.2")
+    implementation("org.bstats:bstats-sponge:3.0.2")
+    implementation("org.bstats:bstats-velocity:3.0.2")
 
     // Gson
     implementation("com.google.code.gson:gson:2.10.1")
+
+    // MySQL
+    implementation("com.zaxxer:HikariCP:4.0.3")
+    implementation("com.mysql:mysql-connector-j:8.3.0")
+
+    // MCLogs
+    implementation("gs.mclo:api:3.0.1")
 
     // Mixin
     compileOnly(libs.mixin)
@@ -30,7 +38,8 @@ dependencies {
     compileOnly("com.mojang:authlib:1.5.25")
 
     // Tooling
-    compileOnly(project(":api"))
+    implementation(project(":api"))
+    implementation(project(":loader"))
     implementation(variantOf(libs.modapi.crossperms) {
         classifier("downgraded-8-shaded")
     })
@@ -41,7 +50,6 @@ dependencies {
         classifier("downgraded-8-shaded")
     })
     compileOnly(project(":modapi:entrypoint-spoof"))
-    implementation(project(":loader"))
 }
 
 java.disableAutoTargetJvm()
@@ -54,11 +62,28 @@ tasks.named<ShadowJar>("shadowJar") {
         exclude("INFO_BIN", "INFO_SRC")
 
         // Tooling
-//        include(project(":api"))
-//        include(project(":loader"))
+        include(project(":api"))
+        include(project(":loader"))
         include(dependency("dev.neuralnexus.modapi:crossperms"))
         include(dependency("dev.neuralnexus.modapi:metadata"))
         include(dependency("dev.neuralnexus.modapi:muxins"))
+
+        // Config
+        include(dependency("org.spongepowered:configurate-core:4.2.0-SNAPSHOT"))
+        include(dependency("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT"))
+        include(dependency("com.typesafe:config:1.4.1"))
+        include(dependency("io.leangen.geantyref:geantyref:1.3.16"))
+
+        // bStats
+        include(dependency("org.bstats:bstats-base:3.0.2"))
+        include(dependency("org.bstats:bstats-bukkit:3.0.2"))
+        include(dependency("org.bstats:bstats-bungeecord:3.0.2"))
+        include(dependency("org.bstats:bstats-sponge:3.0.2"))
+        include(dependency("org.bstats:bstats-velocity:3.0.2"))
+
+        // MySQL
+        include(dependency("com.zaxxer:HikariCP:4.0.3"))
+        include(dependency("com.mysql:mysql-connector-j:8.3.0"))
 
         // MCLogs
         include(dependency("gs.mclo:api:3.0.1"))
@@ -75,15 +100,23 @@ tasks.named<ShadowJar>("shadowJar") {
     relocate("io.leangen.geantyref", "dev.neuralnexus.taterlib.lib.geantyref")
 
     // bStats
-    relocate("org.bstats", "dev.neuralnexus.taterlib.lib.bstats")
+    relocate("org.bstats", "dev.neuralnexus.taterlib.lib.bstats") {
+        exclude("org.bstats.sponge.*")
+    }
 
     // MCLogs
     relocate("gs.mclo", "dev.neuralnexus.taterlib.lib.mclogs")
+
+    // MySQL
+    relocate("com.zaxxer.hikari", "dev.neuralnexus.taterlib.lib.hikari")
+    relocate("com.mysql", "dev.neuralnexus.taterlib.lib.mysql")
 
     // Gson
     relocate("com.google.gson", "dev.neuralnexus.taterlib.lib.gson")
 
     minimize {
+        exclude(project(":api"))
+        exclude(project(":loader"))
         exclude(dependency("dev.neuralnexus.modapi:crossperms"))
         exclude(dependency("dev.neuralnexus.modapi:metadata"))
         exclude(dependency("dev.neuralnexus.modapi:muxins"))
