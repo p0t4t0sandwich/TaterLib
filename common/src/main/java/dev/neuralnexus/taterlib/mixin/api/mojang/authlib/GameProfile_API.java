88c8c3cc7f4febed038a64665c0036db65f78d75
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-05-06 17:09:29 -0600

    Updated GameProfile_API

--- common/src/main/java/dev/neuralnexus/taterlib/mixin/api/mojang/authlib/GameProfile_API.java
+++ common/src/main/java/dev/neuralnexus/taterlib/mixin/api/mojang/authlib/GameProfile_API.java
@@ -8,6 +8,8 @@ import com.mojang.authlib.GameProfile;
 
 import dev.neuralnexus.taterapi.command.CommandSender;
 import dev.neuralnexus.taterapi.entity.Identifiable;
+import dev.neuralnexus.taterapi.entity.Nameable;
+import dev.neuralnexus.taterapi.entity.Notifiable;
 import dev.neuralnexus.taterapi.entity.player.User;
 import dev.neuralnexus.taterapi.meta.enums.MinecraftVersion;
 import dev.neuralnexus.taterapi.muxins.annotations.ReqMCVersion;
@@ -24,34 +26,30 @@ import java.util.UUID;
 @ReqMCVersion(min = MinecraftVersion.V14)
 @Mixin(value = GameProfile.class, remap = false)
 @Implements({
-    @Interface(iface = CommandSender.class, prefix = "cmdSender$", remap = Remap.NONE),
     @Interface(iface = Identifiable.class, prefix = "identifiable$", remap = Remap.NONE),
+    @Interface(iface = Nameable.class, prefix = "nameable$", remap = Remap.NONE),
+    @Interface(iface = Notifiable.class, prefix = "notifiable$", remap = Remap.NONE),
     @Interface(iface = User.class, prefix = "user$", remap = Remap.NONE)
 })
 public abstract class GameProfile_API {
-    @Shadow
-    public abstract String shadow$getName();
+    // @spotless:off
+    @Shadow public abstract String shadow$getName();
+    @Shadow public abstract UUID shadow$getId();
+    // @spotless:on
 
-    @Shadow
-    public abstract UUID shadow$getId();
-
-    public String cmdSender$name() {
+    public String nameable$name() {
         return this.shadow$getName();
-    }
-
-    public void cmdSender$sendMessage(String message) {
-        // TODO: Some abstract way to wrap around and send a message.
-        //   Needs to be as such in order to keep this class platform-agnostic.
-        //   Might update SimplePlayer to send messages to a specific player.
-        //   That way TaterAPIProvider can be used to dispatch the message.
-    }
-
-    public String user$displayName() {
-        return this.cmdSender$name();
     }
 
     public UUID identifiable$uuid() {
         return this.shadow$getId();
+    }
+
+    public void notifiable$sendMessage(String message) {
+        // TODO: Some abstract way to wrap around and send a message.
+        //   Needs to be as such in order to keep this class platform-agnostic.
+        //   Might update SimplePlayer to send messages to a specific player.
+        //   That way TaterAPIProvider can be used to dispatch the message.
     }
 
     public boolean identifiable$hasPermission(String permission) {
