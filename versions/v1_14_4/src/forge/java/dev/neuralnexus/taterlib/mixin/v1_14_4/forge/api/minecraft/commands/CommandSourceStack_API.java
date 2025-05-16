70772dda89b64e905078da4729f6ee7ff60bc6da
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-05-16 01:05:50 -0600

    Further abstracted CommandSource mixins

--- versions/v1_14_4/src/forge/java/dev/neuralnexus/taterlib/mixin/v1_14_4/forge/api/minecraft/commands/CommandSourceStack_API.java
+++ versions/v1_14_4/src/forge/java/dev/neuralnexus/taterlib/mixin/v1_14_4/forge/api/minecraft/commands/CommandSourceStack_API.java
@@ -7,8 +7,6 @@ package dev.neuralnexus.taterlib.mixin.v1_14_4.forge.api.minecraft.commands;
 import dev.neuralnexus.taterapi.annotations.ToBeLibrary;
 import dev.neuralnexus.taterapi.command.CommandSource;
 import dev.neuralnexus.taterapi.entity.Entity;
-import dev.neuralnexus.taterapi.entity.Identifiable;
-import dev.neuralnexus.taterapi.entity.Nameable;
 import dev.neuralnexus.taterapi.entity.Notifiable;
 import dev.neuralnexus.taterapi.meta.Mappings;
 import dev.neuralnexus.taterapi.meta.enums.MinecraftVersion;
@@ -29,16 +27,11 @@ import org.spongepowered.asm.mixin.Shadow;
 @ReqMappings(Mappings.LEGACY_SEARGE)
 @ReqMCVersion(min = MinecraftVersion.V14, max = MinecraftVersion.V16_5)
 @Mixin(CommandSourceStack.class)
-@Implements({
-    @Interface(iface = CommandSource.class, prefix = "source$", remap = Remap.NONE),
-    @Interface(iface = Identifiable.class, prefix = "identifiable$", remap = Remap.NONE),
-    @Interface(iface = Nameable.class, prefix = "nameable$", remap = Remap.NONE),
-    @Interface(iface = Notifiable.class, prefix = "notifiable$", remap = Remap.NONE),
-})
+@Implements(
+    @Interface(iface = CommandSource.class, prefix = "source$", remap = Remap.NONE))
 public abstract class CommandSourceStack_API {
     // @spotless:off
     @Shadow @Final private net.minecraft.commands.CommandSource source;
-    @Shadow public abstract String shadow$getTextName();
     @Shadow @Nullable public abstract net.minecraft.world.entity.Entity shadow$getEntity();
     // @spotless:on
 
@@ -48,9 +41,5 @@ public abstract class CommandSourceStack_API {
 
     public @Nullable Entity source$getEntity() {
         return (Entity) this.shadow$getEntity();
-    }
-
-    public String nameable$name() {
-        return this.shadow$getTextName();
     }
 }
