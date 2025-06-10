7ca8587f815e868150720fad362ba220b7798797
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-06-09 23:49:33 -0600

    Updated brig general's buildscripts, PR time

--- modapi/brigadier-general/bg1_14_4/src/fabric/java/dev/neuralnexus/modapi/brigadier_general/v1_14_4/fabric/BrigGenFabric.java
+++ modapi/brigadier-general/bg1_14_4/src/fabric/java/dev/neuralnexus/modapi/brigadier_general/v1_14_4/fabric/BrigGenFabric.java
@@ -2,23 +2,22 @@
  * Copyright (c) 2025 Dylan Sperrer - dylan@sperrer.ca
  * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE">MIT</a>
  */
-package dev.neuralnexus.modapi.briggen.v1_19_4.fabric;
+package dev.neuralnexus.modapi.brigadier_general.v1_14_4.fabric;
 
-import dev.neuralnexus.modapi.briggen.BrigGenPlugin;
-import dev.neuralnexus.modapi.briggen.EventHelper;
+import dev.neuralnexus.modapi.brigadier_general.BrigGenPlugin;
+import dev.neuralnexus.modapi.brigadier_general.EventHelper;
 import dev.neuralnexus.taterapi.meta.MetaAPI;
 import dev.neuralnexus.taterapi.meta.Platforms;
 
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class BrigGenFabric implements BrigGenPlugin {
     @Override
     public void onInit() {
         if (MetaAPI.instance().isPrimaryPlatform(Platforms.FABRIC)) {
-            // TODO: Load if fabric-command-api-v2
+            // TODO: Load if fabric-command-api-v1
             CommandRegistrationCallback.EVENT.register(
-                    (dispatcher, registryAccess, environment) ->
-                            EventHelper.registerCommand(dispatcher));
+                    (dispatcher, dedicated) -> EventHelper.registerCommand(dispatcher));
         }
     }
 }
