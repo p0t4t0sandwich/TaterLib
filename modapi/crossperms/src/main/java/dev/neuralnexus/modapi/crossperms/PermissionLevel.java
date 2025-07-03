c5d0dcdc520cd68491a7dd7e479a9e0abc7582b0
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-07-03 16:21:24 +1000

    Move wrapped logic and add values for perm levels

---
+++ modapi/crossperms/src/main/java/dev/neuralnexus/modapi/crossperms/PermissionLevel.java
@@ -0,0 +1,9 @@
+package dev.neuralnexus.modapi.crossperms;
+
+public interface PermissionLevel {
+    int ALL = 0;
+    int MODERATORS = 1;
+    int GAMEMASTERS = 2;
+    int ADMINS = 3;
+    int OWNERS = 4;
+}
