2c89d59019eb3aaa59e40a9c7cad8384e3603d47
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-10-07 02:24:54 -0600

    Added new Neo FMLLoader methods

--- modapi/entrypoint-spoof/src/main/java/net/neoforged/fml/loading/FMLLoader.java
+++ modapi/entrypoint-spoof/src/main/java/net/neoforged/fml/loading/FMLLoader.java
@@ -5,16 +5,38 @@
 package net.neoforged.fml.loading;
 
 import net.neoforged.api.distmarker.Dist;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 /** Fake NeoForge class */
 public class FMLLoader {
+    private static AtomicReference<@Nullable FMLLoader> current;
+
     private static Dist dist;
 
     public static VersionInfo versionInfo() {
         return new VersionInfo();
     }
 
+    public VersionInfo getVersionInfo() {
+        return new VersionInfo();
+    }
+
     public static Dist getDist() {
         return dist;
+    }
+
+    public static FMLLoader getCurrent() {
+        @Nullable FMLLoader current = getCurrentOrNull();
+        if (current == null) {
+            throw new IllegalStateException("There is no current FML Loader");
+        }
+        return current;
+    }
+
+    @Nullable
+    public static FMLLoader getCurrentOrNull() {
+        return current.get();
     }
 }
