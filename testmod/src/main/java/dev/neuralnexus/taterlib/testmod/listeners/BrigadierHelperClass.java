101b6d910573af9a057068b72e8b7f98d0506ba4
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-09-01 21:50:16 -0600

    Updated email

--- testmod/src/main/java/dev/neuralnexus/taterlib/testmod/listeners/BrigadierHelperClass.java
+++ testmod/src/main/java/dev/neuralnexus/taterlib/testmod/listeners/BrigadierHelperClass.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2026 Dylan Sperrer - dylan@sperrer.ca
+ * Copyright (c) 2026 Dylan Sperrer - dylan@neuralnexus.dev
  * This project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/main/LICENSE">MIT</a>
  */
 package dev.neuralnexus.taterlib.testmod.listeners;
